7a7b15301903be9d4a6ea298f3d7241f5b807083
AshtonT0726 <ashtontang@gmail.com>
2021-04-18 00:44:02 -0400

    Modify search keywords.

--- src/main/java/org/ashton/mood/MoodEnum.java
+++ src/main/java/org/ashton/mood/MoodEnum.java
@@ -3,9 +3,8 @@ package org.ashton.mood;
 public enum MoodEnum {
   DEPRESSION("(Fight depression OR Win depression)"),
   ANXIETY("(Fight anxiety OR Conquer anxiety OR Overcome anxiety)"),
-  SADNESS("(Fight sadness OR Win sadness)"),
-  NEGATIVE("(Fight negative OR Win negative)"),
-  STRESSED("(Fight stressed OR Win stressed)");
+  SADNESS("(Fight sadness OR Conquer sadness OR positive attitude sadness)"),
+  STRESSED("(Fight stress OR Conquer stress OR positive attitude stress)");
 
   private String searchKey;
 
@@ -18,11 +17,10 @@ public enum MoodEnum {
   }
 
   public static MoodEnum fromValue(String value) {
-    for (MoodEnum e: MoodEnum.values()) {
+    for (MoodEnum e : MoodEnum.values())
       if (e.name().toLowerCase().equals(value.toLowerCase())) {
         return e;
       }
-    }
     return null;
   }
 }
